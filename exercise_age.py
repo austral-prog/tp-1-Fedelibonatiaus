0b9598e0633c192c7415811a6135f974b4277cfa
Federico Libonatu <flibonati@mail.austral.edu.ar>
2026-03-13 12:16:38 -0300

    Add files via upload

--- exercise_age.py
+++ exercise_age.py
@@ -10,13 +10,13 @@ def age():
     4. La edad en minutos (1 hora = 60 minutos)
     """
     edad_anos = 25
-    mes = 1 / 12
-    dia = 1 / 365
-    horas = dia / 24
-    mins = horas / 60
-    print(edad_anos // mes)
-    print(edad_anos // dia)
-    print(edad_anos // horas)
-    print(edad_anos // mins)
-
-#age()
+    meses = edad_anos * 12
+    dias  = edad_anos * 365
+    horas = edad_anos * 8760
+    mins = edad_anos * 525600
+    print(meses)
+    print(dias)
+    print(horas)
+    print(mins)
+    
+age()
